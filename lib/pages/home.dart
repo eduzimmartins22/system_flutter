77dab0337a0757368c49128ca14ec56035834a4e
eduzimmartins22 <Dudukof22@>
2025-05-28 22:28:20 -0300

    .

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -40,7 +40,7 @@ class HomePage extends StatelessWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => const CadastroCliente(),
+                    builder: (context) => const adarstoCliCente(),
                   ),
                 );
               },
